2bd63e917912549ced988f64ea2062b70b39a6ab
Jonny K <Kempj2.jk@gmail.com>
2021-01-18 13:18:58 -0800

    function for removing duplicates from an array

---
+++ arrayNoDups/script.js
@@ -0,0 +1,25 @@
+// PROBLEM:- write a function that will return an array without duplicate values in the array.
+
+const test1 = [1,2,3,4, "foo", "bar"]
+const test2 = [1,1,3,4, "foo","foo", "bar"]
+
+const noDups =  (array) => {
+  const newArray = []
+  newArray.push(array[0])
+  array.forEach(e=>{
+    let count = 0
+    newArray.forEach(j =>{
+      if(j === e){
+        count++
+      }
+    })
+    if(count === 0){
+      newArray.push(e)
+    }
+
+  })
+  console.log(newArray)
+
+}
+
+noDups(test1);
